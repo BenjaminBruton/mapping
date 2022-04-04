fc8cb2c7a57b8b11a427d249f033c835fc5df7ec
BenjaminBruton <bbruton@wgu.edu>
2022-04-04 14:55:58 -0500

    added color fill for diff populations

--- map1.py
+++ map1.py
@@ -1,3 +1,4 @@
+from turtle import fillcolor
 import folium
 import pandas
 
@@ -6,6 +7,14 @@ lat = list(data["LAT"])
 lon = list(data["LON"])
 elev = list(data["ELEV"])
 name = list(data["NAME"])
+
+def color_producer(elevation):
+    if elevation < 1000:
+        return 'green'
+    elif 1000 <= elevation < 3000:
+        return 'orange'
+    else:
+        return 'red'
 
 html = """<strong>Volcano Information:</strong><br>
 <a href="https://www.google.com/search?q=%%22%s%%22" target="_blank">%s</a><br>
@@ -18,7 +27,12 @@ fg = folium.FeatureGroup(name="My Map")
 for lt, ln, el, name in zip(lat, lon, elev, name):
     # fg.add_child(folium.Marker(location=[lt, ln], popup=str(el)+" m", icon=folium.Icon(color='green')))
     iframe = folium.IFrame(html=html % (name, name, el), width=200, height=100)
-    fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon= folium.Icon(color= "green")))
+    fg.add_child(folium.CircleMarker(location=[lt, ln], radius=6, fill_color=color_producer(el), color= "grey",
+        fill_opacity=0.8, popup=folium.Popup(iframe)))
+
+fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
+style_function = lambda x: {'fillColor': 'green' if x['properties']['POP2005'] < 10000000
+else 'orange' if 1000000 <= x['properties']['POP2005'] < 2000000 else 'red'}))
     
 map.add_child(fg)
 
